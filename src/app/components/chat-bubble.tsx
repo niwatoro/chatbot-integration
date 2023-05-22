ca66ec6274b3c8bd83faa4086f50fc74599d4480
niwatoro <girlwithskyhairpin@gmail.com>
2023-05-22 09:44:52 +0900

    update ui

--- src/app/components/chat-bubble.tsx
+++ src/app/components/chat-bubble.tsx
@@ -9,7 +9,7 @@ type Props = {
 export const ChatBubble: FC<Props> = ({ isBot, message, loading }) => {
   return (
     <div className={classNames("w-full", !isBot && "flex justify-end")}>
-      <div className={classNames("p-3.5 rounded-xl tracking-tight w-fit max-w-[260px]", isBot ? "bg-[#ececec] rounded-bl-none" : "bg-[#fae0e0] rounded-br-none")}>
+      <div className={classNames("p-3.5 rounded-xl tracking-tight w-fit max-w-[260px]", isBot ? "bg-[#ececec] rounded-bl-none" : "bg-cyan-100 rounded-br-none")}>
         {loading ? (
           <div className="p-2 w-20 flex justify-center">
             <ThreeDots color="black" height={40} width={40} />
